fcd2fd7e6b097a3a3925471dc642f532d28b8c6d
Andres <andresloa.al@gmail.com>
2021-07-14 00:02:29 -0500

    login fun

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ const exphbs=require('express-handlebars');
 const methoOverride=require('method-override');
 const session=require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
 //init
 const app = express();
 require('./database');
+require('./config/passport');
 //configuraciones
 app.set('port', process.env.PORT||9000);
 app.set('views',path.join(__dirname,'views'));
@@ -25,11 +27,14 @@ app.use(session({
     resave:true,
     saveUninitialized:true
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
 //variables globales
 app.use((req,res,next)=>{
 res.locals.success_msg=req.flash('success_msg');
 res.locals.error_msg=req.flash('error_msg');
+res.locals.error=req.flash('error');
 next();
 });
 
--- src/router/users.js
+++ src/router/users.js
@@ -1,10 +1,51 @@
 const express = require('express');
+const user = require('../model/user');
 const router = express.Router();
-
+const User = require('../model/user');
+const passport = require('passport');
+//ingreso
 router.get('/users/signin',(req,res) => {
     res.render('users/signin');
 });
+router.post('/users/signin',passport.authenticate('local',{
+    successRedirect:'/notes',
+    failureRedirect:'/users/signin',
+    failureFlash:true
+}));
+//registro
 router.get('/users/signup',(req,res) => {
     res.render('users/signup');
+});
+
+router.post('/users/signup',async (req,res)=>{
+const{name, email,password,confpassword}=req.body;
+const errors=[];
+if (name.length <=0) {
+    errors.push({text:'ingrese nombre'});
+    
+}
+if (password != confpassword) {
+    errors.push({text:'Password error// not match'});
+    
+}
+if (password.length<4) {
+    errors.push({text:'La password debe ser mayor  a cuatro caracteres'})   ;   
+}
+if (errors.length > 0) {
+    res.render('users/signup',{errors, name, email,password,confpassword});
+    
+}else{
+   const emailUser = await User.findOne({email: email});
+   if (emailUser) {
+    req.flash('error_msg','El correo ya esta registrado'); 
+    res.redirect('/users/signup');
+   }
+   const newUser = new User({name, email, password});
+   newUser.password = await newUser.encryptPassword(password);
+   await newUser.save();
+   req.flash('success_msg','Registrado');
+   res.redirect('/users/signin');
+}
+
 });
 module.exports = router;
