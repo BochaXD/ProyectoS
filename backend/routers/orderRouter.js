ed4e6b3bd20048c476024af6a35c807ef3ee6922
Andres <andresloa.al@gmail.com>
2021-07-27 12:34:18 -0500

    funca

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -142,7 +142,7 @@ orderRouter.put(
         .messages()
         .send(
           {
-            from: 'Amazona <amazona@mg.yourdomain.com>',
+            from: 'La Veci <laveci@gmail.com>',
             to: `${order.user.name} <${order.user.email}>`,
             subject: `New order ${order._id}`,
             html: payOrderEmailTemplate(order),
